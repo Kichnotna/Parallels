980d35112cb02441abfcc3da21aae78d52ea1f96
Kichnotna <114433365+Kichnotna@users.noreply.github.com>
2024-05-17 21:07:09 +0500

    Load lab2

---
+++ lab2/loop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void heavy_task(int task_num) {
+	printf("tSequential task #%d startedn", task_num);
+
+	int result = 0;
+
+	for (int i = 0; i < 1e8; i++) {
+		result += 1;
+	}
+
+	printf("\tSequential task #%d finishedn", task_num);
+}
+
+
+void loop(int sequence_len) {
+	for (int i = 0; i < sequence_len; i++) {
+		printf("MAIN starting sequential task %dn", i);
+		heavy_task(i);
+	}
+}
+
+int main(int argc, char** argv) {
+	int threads_num = atoi(argv[1]);
+	loop(threads_num);
+
+	return 0;
+}
